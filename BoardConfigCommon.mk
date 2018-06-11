37893d5d73f0dcab023693c9750cef0468779a94
Artem Borisov <dedsa2002@gmail.com>
2018-06-11 12:36:14 +0300

    kirin970-common: Use generic arch configuration
    To be consistent with stock.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,13 +31,13 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := cortex-a15
 
 # Kernel
 BOARD_KERNEL_CMDLINE += loglevel=4 initcall_debug=n page_tracker=on unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538
